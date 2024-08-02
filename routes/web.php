150d067d2a199e2512637274f5530ed9ee10933d
Swhzed364 <eduardkuzmin2000@gmail.com>
2024-08-02 14:26:05 +0300

    depready

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return [
+        'massege' => 'Use api please'
+    ];
 });
 
 Route::get('/login', function () {
